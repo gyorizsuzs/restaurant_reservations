736d91af75aeec6492436528f480fb8f8f8b0eee
gyorizsuzs <106930550+gyorizsuzs@users.noreply.github.com>
2023-03-26 20:00:42 +0200

    creating back navigation

--- webapp/controller/Restaurants.controller.js
+++ webapp/controller/Restaurants.controller.js
@@ -3,8 +3,13 @@ sap.ui.define(["sap/ui/core/mvc/Controller"], function (Controller) {
 
   return Controller.extend("sap.ui.demo.walkthrough.controller.Restaurants", {
     onPress: function (oEvent) {
+      var oItem = oEvent.getSource();
       var oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-      oRouter.navTo("detail");
+      oRouter.navTo("detail", {
+        restaurantPath: window.encodeURIComponent(
+          oItem.getBindingContext("restaurant").getPath().substr(1)
+        ),
+      });
     },
   });
 });
